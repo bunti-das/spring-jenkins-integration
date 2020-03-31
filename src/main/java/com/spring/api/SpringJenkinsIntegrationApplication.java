cfd3ad4854ec60fa38990a1e1464471b44575d1a
bunti.das <bunti.das@accenture.com>
2020-03-31 11:13:58 +0530

    Controller method added

--- src/main/java/com/spring/api/SpringJenkinsIntegrationApplication.java
+++ src/main/java/com/spring/api/SpringJenkinsIntegrationApplication.java
@@ -4,15 +4,23 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SpringJenkinsIntegrationApplication {
 
-	private static final Logger LOGGER = LoggerFactory.getLogger(SpringJenkinsIntegrationApplication.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(SpringJenkinsIntegrationApplication.class);
 
-	public static void main(String[] args) {
-		LOGGER.info("Application started");
-		SpringApplication.run(SpringJenkinsIntegrationApplication.class, args);
-	}
+    public static void main(String[] args) {
+        LOGGER.info("Application started");
+        SpringApplication.run(SpringJenkinsIntegrationApplication.class, args);
+    }
+
+    @GetMapping("/")
+    public String home() {
+        return "Welcome to Spring Jenkins Tutorial";
+    }
 
 }
